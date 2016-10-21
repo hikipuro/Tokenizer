2b92f509a7ad4703a72dad457edd17ebcec478c2
hikipuro <hikipuro@hotmail.com>
2016-10-21 20:58:10 +0900

    TokenList にメソッドを追加

--- Hikipuro/Text/TokenList.cs
+++ Hikipuro/Text/TokenList.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Hikipuro.Text {
 	/// <summary>
@@ -23,6 +24,43 @@ namespace Hikipuro.Text {
 		public Token<TokenType> Last(int index = 0) {
 			int lastIndex = Count - 1;
 			return this[lastIndex - index];
+		}
+
+		/// <summary>
+		/// 2 つのトークン間の距離を, トークンの数で取得する.
+		/// 同じトークン: 0, 隣接しているトークン: 1.
+		/// TODO: IndexOf() が遅そうなので, 高速化する.
+		/// </summary>
+		/// <param name="tokenA">トークン A.</param>
+		/// <param name="tokenB">トークン B.</param>
+		/// <returns>トークン数.</returns>
+		public int GetDistance(Token<TokenType> tokenA, Token<TokenType> tokenB) {
+			int indexA = IndexOf(tokenA);
+			int indexB = IndexOf(tokenB);
+			return Math.Abs(indexA - indexB);
+		}
+
+		/// <summary>
+		/// 2 つのトークンの位置を入れ替える.
+		/// TODO: IndexOf() が遅そうなので, 高速化する.
+		/// </summary>
+		/// <param name="tokenA">トークン A.</param>
+		/// <param name="tokenB">トークン B.</param>
+		public void Swap(Token<TokenType> tokenA, Token<TokenType> tokenB) {
+			int indexA = IndexOf(tokenA);
+			int indexB = IndexOf(tokenB);
+			Swap(indexA, indexB);
+		}
+
+		/// <summary>
+		/// 2 つのトークンの位置を入れ替える.
+		/// </summary>
+		/// <param name="indexA">トークン A のインデックス.</param>
+		/// <param name="indexB">トークン B のインデックス.</param>
+		public void Swap(int indexA, int indexB) {
+			Token<TokenType> token = this[indexA];
+			this[indexA] = this[indexB];
+			this[indexB] = token;
 		}
 	}
 }
